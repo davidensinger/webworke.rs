3ce673871c05bd10cb71a4b6f378b197b1452e8e
David Ensinger <david@webworke.rs>
2013-11-09 19:39:32 -0500

    Drop source map support from Grunt Sass serve as I’ve downgraded Sass to 3.2. Also remove extra % from Gruntfile.js tag.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
         files: [
           '<%= yeoman.app %>/**/*.{html,yml,xml}',
           '_config.yml',
-          '!<%%= yeoman.app %>/_bower_components/**'
+          '!<%= yeoman.app %>/_bower_components/**'
         ],
         tasks: ['jekyll:server']
       },
@@ -114,7 +114,6 @@ module.exports = function (grunt) {
       },
       server: {
         options: {
-          sourcemap: true,
           lineNumbers: true
         },
         files: [{
