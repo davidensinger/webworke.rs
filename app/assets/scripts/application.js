1bcbe71c25500be7cf56333daa83459c60e73066
David Ensinger <david@webworke.rs>
2013-11-20 23:24:42 -0500

    Initial styles for the left and right posts navigation arrows.

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -3,7 +3,7 @@ Mousetrap.bind('left', keyboardNavigation.bind(null, 'previous'))
 Mousetrap.bind('right', keyboardNavigation.bind(null, 'next'))
 
 function keyboardNavigation(ID) {
-  var element = document.getElementById('js-navigation-' + ID);
+  var element = document.getElementById('js-nav-' + ID);
   if (element) {
     var anchor = element.getAttribute('href');
     window.location.href = anchor;
