0959c6cbbcf1e981605c60023980eefdcbb4db09
David Ensinger <david@webworke.rs>
2013-11-22 11:34:39 -0500

    Make the left and right keyboard navigation display :focus state in application.js.

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -1,12 +1,13 @@
 // Keyboard navigation, courtesy of Mousetrap.js (http://craig.is/killing/mice)
-Mousetrap.bind('left', keyboardNavigation.bind(null, 'previous'))
-Mousetrap.bind('right', keyboardNavigation.bind(null, 'next'))
+Mousetrap.bind('left', keyboardNavigation.bind(null, 'previous'));
+Mousetrap.bind('right', keyboardNavigation.bind(null, 'next'));
 
 function keyboardNavigation(ID) {
   var element = document.getElementById('js-nav-' + ID);
   if (element) {
+    element.focus();
     var anchor = element.getAttribute('href');
     window.location.href = anchor;
-    return false
+    return false;
   }
 }
