d01139b2705b86a31004404bd6aef81207771857
David Ensinger <david@webworke.rs>
2013-11-28 17:29:21 -0500

    Standardize the naming convention to camelCase-camelCase--camelCase__camelCase across all templates and stylesheets.

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -3,7 +3,7 @@ Mousetrap.bind('left', keyboardNavigation.bind(null, 'previous'));
 Mousetrap.bind('right', keyboardNavigation.bind(null, 'next'));
 
 function keyboardNavigation(ID) {
-  var element = document.getElementById('js-nav-' + ID);
+  var element = document.getElementById('js-navigation--' + ID);
   if (element) {
     element.focus();
     var anchor = element.getAttribute('href');
