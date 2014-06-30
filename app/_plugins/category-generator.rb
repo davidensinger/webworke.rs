ffd80b33aeafcab8449fdf912f70c899c1dd96c6
David Ensinger <david@webworke.rs>
2014-06-30 14:15:16 -0400

    Remove pagination from category-generator.rb because we don’t use it and it throws an error anyhow.

--- app/_plugins/category-generator.rb
+++ app/_plugins/category-generator.rb
@@ -13,28 +13,12 @@ module Jekyll
     def build_subpages(site, type, posts)
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }
       atomize(site, type, posts)
-      paginate(site, type, posts)
     end
 
     def atomize(site, type, posts)
       path = "/#{type}/#{posts[0]}".downcase.strip.gsub(' ', '-')
       atom = AtomPage.new(site, site.source, path, type, posts[0], posts[1])
       site.pages << atom
-    end
-
-    def paginate(site, type, posts)
-      pages = Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
-      (1..pages).each do |num_page|
-        pager = Pager.new(site, num_page, posts[1], pages)
-        path = "/#{type}/#{posts[0]}".downcase.strip.gsub(' ', '-')
-        if num_page > 1
-          path = path + "/page#{num_page}"
-        end
-        newpage = GroupSubPage.new(site, site.source, path, type, posts[0])
-        newpage.pager = pager
-        site.pages << newpage
-
-      end
     end
   end
 
