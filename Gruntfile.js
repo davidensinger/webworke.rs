b316b66c58c6b55a3eb877b79064f11101eca2eb
David Ensinger <david@webworke.rs>
2014-12-07 15:11:26 -0500

    Add perf/ directory to be excluded by the htmlmin task in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -177,7 +177,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.dist %>',
-          src: '**/*.html',
+          src: ['**/*.html', '!perf/**/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
