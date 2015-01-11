ee86e4b3109866a84e3bdba821a9d7dfc8895274
David Ensinger <david@webworke.rs>
2015-01-11 16:34:02 -0500

    Remove DOMContentLoaded event listener from JS in scripts.js

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,35 +1,32 @@
 /* global smoothScroll, responsiveFootnotes */
 
-document.addEventListener('DOMContentLoaded', function(){
-
-  // Keyboard navigation
-  function keyboardNavigation(ID) {
-    var element = document.getElementById('js-navigation--' + ID);
-    if (element) {
-      element.className = element.className + ' is-active';
-      var anchor = element.getAttribute('href');
-      window.location.href = anchor;
-      return false;
-    }
+// Keyboard navigation
+function keyboardNavigation(ID) {
+  var element = document.getElementById('js-navigation--' + ID);
+  if (element) {
+    element.className = element.className + ' is-active';
+    var anchor = element.getAttribute('href');
+    window.location.href = anchor;
+    return false;
   }
+}
 
-  document.onkeydown = function(e) {
-    e = e || window.event;
-    switch(e.which || e.keyCode) {
-      case 37: // left
-        keyboardNavigation('previous');
-        break;
-      case 39: // right
-        keyboardNavigation('next');
-        break;
-      default: return;
-    }
-    e.preventDefault();
-  };
+document.onkeydown = function(e) {
+  e = e || window.event;
+  switch(e.which || e.keyCode) {
+    case 37: // left
+      keyboardNavigation('previous');
+      break;
+    case 39: // right
+      keyboardNavigation('next');
+      break;
+    default: return;
+  }
+  e.preventDefault();
+};
 
-  // Smooth scroll
-  smoothScroll.init();
+// Smooth scroll
+smoothScroll.init();
 
-  // Responsive footnotes
-  responsiveFootnotes.init();
-});
+// Responsive footnotes
+responsiveFootnotes.init();
