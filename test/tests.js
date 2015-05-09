390af0b5bee33a2e84ade514632cc5408b7d708d
David Ensinger <david@webworke.rs>
2015-05-09 11:56:47 -0400

    Remove left and right arrow keyboard navigation given all the bug reports.

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,3 @@
-var keyboardNavigation = require('keyboard-navigation');
 var responsiveFootnotes = require('responsive-footnotes');
 
-keyboardNavigation.keyboardNavigationInit();
 responsiveFootnotes.responsiveFootnotesInit();
